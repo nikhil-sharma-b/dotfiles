5e8b6da9b9c3449aa0df24c7716f306fa3cfc1a3
nikhil-sharma-b <nikhil.sharma.colab@gmail.com>
2026-04-04 13:12:06 +0530

    Add necessary aliases and tmux abbreviations

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,5 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    alias c="clear"
+    alias oc="opencode"
+    alias lg="lazygit"
+    alias q="exit"
+    alias cc="claude --dangerously-skip-permissions"
+
+    abbr -a tl "tmux ls"
+    abbr -a ta "tmux a -t"
+    abbr -a tn "tmux new -s"
 
     source ~/.config/fish/functions/yy.fish
 end
